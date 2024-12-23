ecf9e88c769b913aed6c4140ec0313545dd8a75b
pabitra3362 <pmohanty1435@gmail.com>
2024-12-23 22:35:49 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     proxy: {
       // Proxy API requests to the backend server on port 3000
       '/api': {
-        target: 'http://localhost:3000/api',  // Your backend server URL
+        target: 'https://cafelinbackend.vercel.app/api',  // Your backend server URL
         changeOrigin: true,  // Needed for virtual hosted sites
-        secure: false,      // Set to true if you have SSL, false if not
+        secure: true,      // Set to true if you have SSL, false if not
         rewrite: (path) => path.replace(/^\/api/, ''),  // Optional: Remove '/api' prefix if necessary
       },
     },
