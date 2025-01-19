f97a39607eb31f6402d3521bb46b41efb768c905
pabitra3362 <pmohanty1435@gmail.com>
2025-01-19 19:48:20 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     // Proxy API requests to the backend server on port 3000
-  //     '/api': {
-  //       target: 'https://cafelin.onrender.com/api',  // Your backend server URL
-  //       changeOrigin: true,  // Needed for virtual hosted sites
-  //       secure: true,      // Set to true if you have SSL, false if not
-  //       rewrite: (path) => path.replace(/^\/api/, ''),  // Optional: Remove '/api' prefix if necessary
-  //     },
-  //   },
-  // },
 })
