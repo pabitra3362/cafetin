9d36b310f70e14e2815931925dfaca9c7d0f74dd
pabitra3362 <pmohanty1435@gmail.com>
2025-01-24 16:52:36 +0530

    .

--- frontend/src/components/DishCard.jsx
+++ frontend/src/components/DishCard.jsx
@@ -1,33 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { motion } from 'motion/react'
-import { TfiPlus } from "react-icons/tfi";
 import { FaCartPlus } from "react-icons/fa6";
 import { useAuth0 } from "@auth0/auth0-react";
 import { ToastContainer, toast } from 'react-toastify';
-import addCartLoader from '../assets/addCartLoader.gif';
 import axios from 'axios'
 
 
 const DishCard = ({image,title,description,price}) => {
 
   const {user,isAuthenticated}=useAuth0()
-  const [spinner, setSpinner] = useState(false);
-
-  useEffect(()=>{
-    const timer = ()=>{
-      setTimeout(() => {
-        setSpinner(false);
-      }, 2000);
-    }
-
-    timer();
-
-    return ()=> clearTimeout(timer);
-  },[spinner])
 
   const handleCart=async() => {
     if(!isAuthenticated) toast.error("Please login then try again !!!")
-    setSpinner(true);
     try {
       const res=await axios.post('https://cafelin.onrender.com/api/cart/addToCart',{image,title,description,price,user:user.name,quantity:1})
       const data= await res.data;
@@ -58,12 +42,9 @@ const DishCard = ({image,title,description,price}) => {
     className='w-80 h-[450px] bg-[#f5f4f4] rounded-lg grid grid-cols-1 justify-center p-2 gap-3 drop-shadow-xl'>
         <div className='relative'>
           <img src={image} className='h-56  w-80 rounded-lg ' alt="" />
-          <button 
-          disabled={spinner}
+          <button
           onClick={handleCart} title='click to add item in the cart' className='absolute p-2 rounded-full bg-white text-xl bottom-5 right-3'>
-            {
-              spinner ? (<img src={addCartLoader} className='w-6 h-6'/>) : <FaCartPlus />
-            }
+             <FaCartPlus />
           </button>
         </div>
         <p className='font-bold text-2xl'>{title}</p>
